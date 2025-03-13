ea983236fc5b5ae4c023963a666be20c3e885902
darshanN04 <darshannakshatri@gmail.com>
2025-03-13 23:25:20 +0530

    login

--- app/(pages)/Profile.tsx
+++ app/(pages)/Profile.tsx
@@ -1,10 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { supabase } from '@/lib/supabase'
+import { Link } from 'expo-router'
 
 const Profile = () => {
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut()
+    
+  }   
+
+
   return (
     <View>
       <Text>Profile</Text>
+
+      <View>
+        <Link href="../Auth">
+          <TouchableOpacity onPress={handleSignOut}>
+            <Text>Sign Out</Text>
+          </TouchableOpacity>
+        </Link>
+        
+      </View>
     </View>
   )
 }
